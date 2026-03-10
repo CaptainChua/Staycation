86ecb4a94ca05406f35a9de8f631774f26d15655
sheila merida <sheilameridaa@gmail.com>
2026-03-10 09:14:55 +0800

    fixed cannot click view all activity logs

---
+++ app/admin/csr/activity-logs/page.tsx
@@ -0,0 +1,10 @@
+import ActivityLogsPage from "@/Components/admin/Csr/ActivityLogsPage";
+
+export default function Page() {
+	return (
+		<div className="p-6">
+			<ActivityLogsPage />
+		</div>
+	);
+}
+
