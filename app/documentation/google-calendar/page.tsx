d1a7f82093737e52fb6d5b465c0b1c9ce6fa1caf
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-03-21 12:13:26 +0800

    create page for google calendar

---
+++ app/documentation/google-calendar/page.tsx
@@ -0,0 +1,10 @@
+import GitWorkflowLayout from '@/Components/Documentation/GitWorkflowLayout';
+import GoogleCalendarContent from '@/Components/Documentation/GoogleCalendarContent';
+
+export default function GoogleCalendarPage() {
+  return (
+    <GitWorkflowLayout>
+      <GoogleCalendarContent />
+    </GitWorkflowLayout>
+  );
+}
