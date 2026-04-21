809906c4deea3b6f31b77f7209aa3c112b170739
sheila merida <sheilameridaa@gmail.com>
2026-04-21 13:04:34 +0800

    Reviews and Feedback

--- redux/api/reviewsApi.ts
+++ redux/api/reviewsApi.ts
@@ -37,6 +37,8 @@ export interface Review {
   created_at: string;
   check_in_date?: string;
   check_out_date?: string;
+  owner_response?: string;
+  owner_response_at?: string;
 }
 
 export interface ReviewsResponse {
